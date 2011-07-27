9dcfed574f18bcd6d12bbccbf0915c04c90c6280
Nelson Chen <crazysim@gmail.com>
2011-07-26 17:25:02 -0700

    ARCAized application class

--- src/com/ferg/awful/AwfulApplication.java
+++ src/com/ferg/awful/AwfulApplication.java
@@ -1,6 +1,8 @@
 package com.ferg.awful;
 
 import android.app.Application;
+import org.acra.*;
+import org.acra.annotation.*;
 
 /**
  * Class originally declared to allowed application-wide access to the global image cache.
@@ -8,7 +10,15 @@ import android.app.Application;
  *
  * @author brosmike
  */
+
+@ReportsCrashes(formKey = "dFBFM1pxcWw0ZUFoU1J4b2FNRFVaTWc6MQ")
 public class AwfulApplication extends Application {
 	private static String TAG="AwfulApplication";
 
+     @Override
+    public void onCreate() {
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+        super.onCreate();
+    }
 }
